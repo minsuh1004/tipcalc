f96d66ac480e10dcf97e1982359952dfb56eb07e
minsuh1004 <immsk812@gmail.com>
2023-10-25 16:57:18 -0700

    Completed app and added spinner with tip percentages

--- app/src/main/java/edu/uw/ischool/msk812/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/msk812/tipcalc/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     lateinit var amountEdit: EditText
     lateinit var tipButton: Button
+    lateinit var tipPercent: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         amountEdit = findViewById(R.id.amountText)
         tipButton = findViewById(R.id.tipButton)
+        tipPercent = findViewById(R.id.tipSpinner)
+
         tipButton.isEnabled = false
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.percent_array,
+            android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tipPercent.adapter = adapter
+
 
         amountEdit.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -28,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable?) {
                 val input = s.toString()
-                if (input.isNotEmpty() && input.matches(Regex("^[0-9]*\\.[0-9]{2}\$"))) {
+                if (input.isNotEmpty() && input.matches(Regex("^(\\d*\\.)?\\d+\$"))) {
                     tipButton.isEnabled = true
                 } else {
                     tipButton.isEnabled = false
@@ -38,7 +50,8 @@ class MainActivity : AppCompatActivity() {
 
         tipButton.setOnClickListener {
             val amount = amountEdit.text.toString().toDouble()
-            val tipAmount = amount * 0.15
+            val convertPercent = tipPercent.selectedItem.toString().replace("%", "").toDouble()
+            val tipAmount = amount * (convertPercent / 100)
             val formatTip = NumberFormat.getCurrencyInstance().format(tipAmount)
             val toastMessage = "Tip amount $formatTip"
             val toast = Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT)
